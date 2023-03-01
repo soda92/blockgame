84a871d20312a6d5e7cabe89ea9715dfc2c3a39d
SodaCris <wang-jiaxun@outlook.com>
2023-03-01 22:46:04 +0800

    restructure

---
+++ src/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).resolve().parent
+IMG_Path = CURRENT_DIR.parent.joinpath("resources").joinpath("girl.jpeg")
+X_Splits = 4
---
+++ src/main.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+
+import tools
+import config
+
+if __name__ == "__main__":
+    pygame.init()
+    picrect = pygame.image.load(config.IMG_Path).get_rect()
+    screen = pygame.display.set_mode((picrect.width, picrect.height))
+
+    black = 0, 0, 0
+    image_sequences = tools.cut_image(config.IMG_Path, config.X_Splits)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(black)
+        for image, box in image_sequences:
+            screen.blit(image, box)
+
+        pygame.display.flip()
