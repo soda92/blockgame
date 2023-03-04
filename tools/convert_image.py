66848eae58cdfabaf29fa4ca7623ad133dc923a3
SodaCris <wang-jiaxun@outlook.com>
2023-03-04 09:39:01 +0800

    polish image conversion 1. use `Path.glob` instead of `os.listdir` 2. use `Path.mkdir` instead of `os.mkdir` 3. remove unnecessary `str(Path)` 4. use `Path.stem` instead of manual split filename 5. add error tolerant

--- tools/convert_image.py
+++ tools/convert_image.py
@@ -1,20 +1,21 @@
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from pathlib import Path
 
 CURRENT = Path(__file__).resolve().parent
 RES_DIR = CURRENT.parent.joinpath("resources")
-
-import os
-files = os.listdir(str(RES_DIR))
-files = [RES_DIR.joinpath(fn) for fn in files]
 DEST_DIR = RES_DIR.joinpath("converted")
-if not DEST_DIR.exists():
-    os.mkdir(str(DEST_DIR))
+DEST_DIR.mkdir(exist_ok=True)
+
+files = RES_DIR.glob("*")
+files = list(filter(Path.is_file, files))
+
 
 for file in files:
-    if not file.is_file(): continue
-    im = Image.open(file)
-    fn_noext = os.path.splitext(file.name)[0]
-    im_png = fn_noext + ".png"
-    im_png = DEST_DIR.joinpath(im_png)
-    im.save(str(im_png))
+    try:
+        im = Image.open(file)
+    except UnidentifiedImageError:
+        continue
+    else:
+        im_png = file.stem + ".png"
+        im_png = DEST_DIR.joinpath(im_png)
+        im.save(im_png)
